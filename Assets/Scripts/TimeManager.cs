ad87d7bbd5df9b5b8392c70c77adb06b68206678
yisy0713 <yisy0713@naver.com>
2024-04-01 11:54:02 +0900

    낮/밤 판별 & 밤에 플레이어근처에 몬스터 스폰

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -12,7 +12,7 @@ public class TimeManager : MonoBehaviour
     private int currentDayCount = 0;
     //public static bool stop = false;
 
-    private bool isNight = false;
+    private bool isNight = true;
 
     [SerializeField]
     private GameObject directionalLight;
@@ -26,6 +26,18 @@ public class TimeManager : MonoBehaviour
     [SerializeField]
     private GameObject dayCounter;
     private TextMeshProUGUI dayCounterText;
+
+    [SerializeField]
+    private GameObject monsterPrefab;
+
+    [SerializeField]
+    private float spawnRadius = 10f; // 스폰 반경
+    [SerializeField]
+    private float spawnInterval = 10f; // 스폰 간격
+    private float nextSpawnTime = 0f;
+
+    [SerializeField]
+    private Transform player;
 
     // Start is called before the first frame update
     void Start()
@@ -46,7 +58,35 @@ public class TimeManager : MonoBehaviour
 
         //if((int)(currentTime / 30))
 
+        if ((currentTime - (60 * currentDayCount) < 30) && isNight) // 낮이 되었을 때
+        {
+            isNight = false;
+            Debug.Log("Day");
+            // 여기에 낮에 필요한 작업 추가
+        }
+        else if ((currentTime - (60 * currentDayCount) >= 30) && !isNight) // 밤이 되었을 때
+        {
+            isNight = true;
+            Debug.Log("Night");
+            // 여기에 밤에 필요한 작업 추가
+            SpawnMonsters();
+        }
+
+        
+
         UpdateUI();
+    }
+    void SpawnMonsters()
+    {
+        if (Time.time >= nextSpawnTime)
+        {
+            // 플레이어 주변에 몬스터 스폰
+            Vector3 spawnOffset = Random.insideUnitSphere * spawnRadius;
+            Vector3 spawnPosition = player.position + spawnOffset;
+            spawnPosition.y = 0;
+            Instantiate(monsterPrefab, spawnPosition, Quaternion.identity);
+            nextSpawnTime = Time.time + spawnInterval;
+        }
     }
 
     void UpdateUI()
