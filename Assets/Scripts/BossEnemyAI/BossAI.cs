469ad869295c416faf8ec1b60567965630ff3e5a
yisy0713 <yisy0713@naver.com>
2024-04-10 23:51:09 +0900

    보스AI초기작업

---
+++ Assets/Scripts/BossEnemyAI/BossAI.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using BehaviorTree;
+
+public class BossAI : Tree
+{
+    public UnityEngine.Transform[] wayPoints;
+    public UnityEngine.Transform returnPoint;
+    public UnityEngine.Rigidbody rigid;
+
+    public static float speed = 1f;
+    public static float runSpeed = 3f;
+    private float fovRange = 8f;
+    private float attackRange = 1.2f;
+
+    public static float timer = 0f;
+    protected override Node SetupTree()
+    {
+        Node root = new Selector(new List<Node>
+        {
+            new CheckAmIDead(transform),
+            new Sequence(new List<Node>
+            {
+                new CheckPlayerInFovRange(transform, attackRange),
+                new TaskAttack(transform),
+            }),
+            new Sequence(new List<Node>
+            {
+                new CheckPlayerInFovRange(transform, fovRange),
+                new TaskGoToTarget(transform, rigid),
+            }),
+            new Sequence(new List<Node>
+            {
+                new TaskWalk(transform, rigid),
+            }),
+        });
+
+        return root;
+    }
+}
